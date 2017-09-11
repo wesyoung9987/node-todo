f3c6dbe1a759ddfb09e6ac66527060b72324eba5
Wes Young <wes.young14@gmail.com>
2017-09-11 16:57:41 -0600

    Updated mongodb

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    // remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://wesyoung2:snow_boarding1@ec2-52-72-36-47.compute-1.amazonaws.com:27017/dummyDB',
+    // localUrl: 'mongodb://localhost/meanstacktutorials'
 };
